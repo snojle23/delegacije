f0028109ff3994da5fb7ef073d56584e3577962d
Tadej Snoj <tadej.snoj@adacta-group.com>
2019-11-04 23:33:55 +0100

    1.0

--- slo.js
+++ slo.js
@@ -2,26 +2,100 @@ const exceljs = require('exceljs');
 const fs = require('fs')
 
 
-	async function odpriExcel() {
+	 function odpriExcel() {
         var workbook = new exceljs.Workbook();
-        workbook = await workbook.xlsx.readFile('AHLdelegacije.xlsx');
         
-        var worksheet = workbook.getWorksheet('AHL');
-        var count = 1;   
-         while(1){
+        workbook.xlsx.readFile('data/AHLdelegacije.xlsx').then(workbook =>{
 
+            
+        
+            let worksheet = workbook.getWorksheet('AHL');
+            
+            var count = 1;   
+            let countGSlo =0;
+            let countLSlo = 0;
+            
+            let countGAu =0;
+            let countLAu = 0;
+            let holzer = 0;
+            let huber = 0;
+            let brata = 0;
+            
+            let countGIta =0;
+            let countLIta = 0;
+             while(worksheet.getRow(3).getCell(count).value != null){
+                   
                 if(worksheet.getRow(1).getCell(count).value == "slo" ){
-                    console.log("sodnik " + worksheet.getRow(3).getCell(count).value + " tekm: " + worksheet.getRow(2).getCell(count).value.result );
+                    if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FFFF0000"){
+                        countGSlo+=worksheet.getRow(2).getCell(count).value.result;
+                    }
+                    else if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FF00B0F0"){
+                        countLSlo+=worksheet.getRow(2).getCell(count).value.result;
+                        
+                    }
+                }
+                else if(worksheet.getRow(1).getCell(count).value == "au" ){
+                    if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FFFF0000"){
+                        countGAu+=worksheet.getRow(2).getCell(count).value.result;
+                    }
+                    else if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FF00B0F0"){
+                        countLAu+=worksheet.getRow(2).getCell(count).value.result;
+                    }
+
+                }
+                else if(worksheet.getRow(1).getCell(count).value == "ita" ){
+                    if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FFFF0000"){
+                        countGIta+=worksheet.getRow(2).getCell(count).value.result;
+                      //  console.log(countGlavniIta);
+                    }
+                    else if(worksheet.getRow(3).getCell(count).fill.fgColor.argb == "FF00B0F0"){
+                        countLIta+=worksheet.getRow(2).getCell(count).value.result;
+                    }
+                    else if(worksheet.getRow(3).getCell(count).value = "GIACOMOZZI"){
+                        brata += worksheet.getRow(2).getCell(count).value.result;
+                    }
+                }
+                else if(worksheet.getRow(1).getCell(count).value == "au?"){
+                    if(worksheet.getRow(3).getCell(count).value == "HOLZER"){
+                        
+                        holzer += worksheet.getRow(2).getCell(count).value.result;
+                    }
+                    else if(worksheet.getRow(3).getCell(count).value == "HUBER"){
+                        huber += worksheet.getRow(2).getCell(count).value.result;
+                    }
+                }
+                else if(count >200){
+                    return 1;
                     
                 }
-            else if(count >200){
-                return;
+                count++;
+               
             }
-            count++;
-        }
+            console.log("Glavni slo: "+ countGSlo);
+            console.log("Linijski slo: "+ countLSlo);
+            console.log("Glavni Au: "+ countGAu);
+            console.log("Linijski Au: "+ countLAu);
+            console.log("Glavni Ita: "+ countGIta);
+            console.log("Linijski Ita: "+ countLIta);
+            console.log("HOLzer :" + holzer);
+            console.log("huber :" + huber);
+            console.log("giacomozi :" + brata);
+            console.log("vsi glavni " + (countGAu + countGIta + countGSlo))
+            console.log("vsi liniski "+ (countLAu + countLIta +countLSlo) )
+            let vsi = countLAu + countLIta +countLSlo +countGAu + countGIta + countGSlo;
+            let tekm = vsi / 4 ;
 
-       
+            console.log (vsi + " tekm : "+tekm)
+            console.log ( (countLSlo+ countGSlo) /437);
+            console.log ( (countLAu+ countGAu) /437);
+            console.log ( (countLIta+ countGIta) /437);
+        }
+            )
+
+ 
     }
+
+
 
    odpriExcel();
 
