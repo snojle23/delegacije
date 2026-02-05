0eefee85ffbf868e17612921feb87cf448da9c09
Tadej Snoj <tadej.snoj@adacta-fintech.com>
2026-02-05 11:04:16 +0100

    5.2.

--- delegacije.js
+++ delegacije.js
@@ -26,7 +26,7 @@ async function delegacije(json_data, arrayForMail) {
     const workICEHL_DAT_STRING = await icehlWorkbook.getWorksheet('ICEHL_DAT_STRING');
     const noviDatumiAHL = [];
     const noviDatumiICEHL = [];
-    const filterDates = ['2026-02-12'];
+    const filterDates = ['2026-02-12', '2026-02-14'];
 
     (json_data || []).filter(i => !filterDates.includes(i.datum)).forEach(elementDate => { // gre cez use datume
         let datum = elementDate.datum;
